7f3f7482425af42318b7c671fb2a6542f9bffd88
Anuj Mehla <anujmehla@gmail.com>
2024-02-16 17:28:14 +0530

    Initial Commit

---
+++ src/main/java/com/anujmehla/My/Story/MyStoryApplication.java
@@ -0,0 +1,13 @@
+package com.anujmehla.My.Story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoryApplication.class, args);
+	}
+
+}
